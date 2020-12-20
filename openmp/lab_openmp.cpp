f8d3d4e4bd114d8144e0672c1f38db25f9e9032c
alexander161198 <sasha.lubyanov@mail.ru>
2020-12-20 21:30:00 +0300

    add openmp file

--- openmp/lab_openmp.cpp
+++ openmp/lab_openmp.cpp
@@ -1,210 +1,280 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include<stdio.h>
+#include<iostream> 
+#include<stdlib.h> 
+#include<math.h> 
+#include<cmath> 
 #include <omp.h>
-#include <time.h>
-#include "inverse_openmp.h"
-#include "boost/numeric/ublas/matrix.hpp"
-#include "boost/numeric/ublas/vector.hpp"
-using namespace boost::numeric::ublas;
 
+using namespace std; 
 
-//граничное условие 2 рода dT/dn=10
-#define TT 10
+#define X 32 
+#define Y 32 
+#define X_IN 10 
+#define Y_IN 10
 
-matrix <double> all_calc (double a1,double a2,double L1,double L2,int N_i,int N_j, int k) {
-	double x1 = (L1-a1)/2;
-    double y1 = (L2-a2)/2; 
-    double x2 = (L1+a1)/2; 
-    double y2 = (L2+a2)/2; 
+#define N ((X+1) * (Y+1)) 
+#define TIME 10
 
-    double dx = L1/(N_i - 1);
-    double dy = L2/(N_j - 1);
+double h_x = 0.2; 
+double h_y = 0.2; 
+double h_t = 1;
 
-    //%Инициализируем начальные массивы
+void gauss(double** matrix, double solve[], int n)
+{ 
+	int j1;
+	double **matr = new double *[n]; 
+	for (int i = 0; i < n; i++) 
+		matr[i] = new double [n];
+	double slv[N];
 
-    matrix<double> M (N_i*N_j,N_i*N_j);
-    vector<double> R (N_i*N_j);
-
-    for (int i = 0; i < N_i; i++) {
-        for (int j = 0; j < N_j; j++) {
-        	//printf("%d\n", omp_get_thread_num());
-            double x=i*dx;
-            double y=j*dy;
-            if (i==0) { //самая левая граница dT/dn=10
-                M(j+N_j*(i),j+N_j*(i+1)) = 1/dx; //i+1, j
-                M(j+N_j*(i),j+N_j*(i)) = -1/dx;//i,j
-                R(j + N_j * (i)) = TT;
-                continue;
-            }
-
-            if (j==0){	//нижняя dT/dn=10
-            	M(j+N_j*(i),j+N_j*(i)) = -1/dy;//i, j,
-                M(j+N_j*(i),j+N_j*(i)+1) = 1/dy;//i,j+1
-                R(j + N_j * (i)) = TT;
-            	continue;
-            }
-            if (j==N_j-1) { //%самая верхняя граница dT/dn =10
-            	M(j+N_j*(i),j+N_j*(i)) = -1/dy;//i, j,
-                M(j+N_j*(i),j+N_j*(i)-1) = 1/dy;//i,j-1
-                R(j + N_j * (i)) = TT;
-                continue;
-            }
-            if (i==N_i-1) { //самая правая граница dT/dn=10
-            	M(j+N_j*(i), j+N_j*(i-1)) = 1/dx; //i-1, j
-                M(j+N_j*(i),j+N_j*(i)) = -1/dx;//i,j
-                R(j + N_j * (i)) = TT;
-                continue;
-            }
-            //внутренняя граница нижняя
-            if ((y<=y1) &&((y+dy)>y1) && (x<=x2) &&(x>=x1)) { 
-                M(j + N_j * (i),j + N_j * (i)) = 1;//i, j
-                R(j + N_j * (i)) = 100;
-                continue;
-            }
-            //внутренняя граница верхняя
-            if ((y>y2) &&((y-dy)<y2) && (x<=x2) && (x>=x1)) { 
-                M(j + N_j * (i),j + N_j * (i)) = 1;//i, j
-                R(j + N_j * (i)) = 100;
-                continue;
-            }
-            //внутренняя граница правая
-            if (((x-dx)<x2) &&(x>=x2) && (y<=y2) &&(y>=y1)) {
-            	M(j + N_j * (i),j + N_j * (i)) = 1;//i, j
-                R(j + N_j * (i)) = 100;
-                continue;
-            }
-            //внутренняя граница левая
-            if (((x+dx)>x1) &&(x<=x1) && (y<=y2) &&(y>=y1)) { 
-                M(j + N_j * (i),j + N_j * (i)) = 1;//i, j
-                R(j + N_j * (i)) = 100;
-                continue;
-            }
-
-            if ((x>=x1) && (x<=x2) && (y<=y2) && (y>=y1)) {
-				M(j + N_j * (i),j + N_j * (i)) = 1;//i, j
-                R(j + N_j * (i)) = 100;
-                continue;
-            }
-            if ((x<=x1) || (x>=x2) || ((x>x1 && x<x2) && ((y<=y1) || (y>=y2)))) { //%Основная часть
-                M(j+N_j*(i),j+N_j*(i+1)) =a1/(dx*dx); //i+1, j
-                M(j+N_j*(i),j+N_j*(i)) = -2*a1/(dx*dx)-2*a2/(dy*dy);//i,j
-                M(j+N_j*(i),j+N_j*(i-1)) = a1/(dx*dx);//i-1, j,
-                M(j+N_j*(i),j+N_j*(i)+1) =a2/(dy*dy);//i,j+1
-                M(j+N_j*(i),j+N_j*(i)-1) =a2/(dy*dy);//i,j-1
-                continue;
-            }
-            M(j+N_j*(i),j+N_j*(i)) = 1;//i,j
-            R(j+N_j*(i)) = 0;
-        }
-    }
-
-	/*clock_t start = clock();*/
-
-    matrix<double> iM (N_i*N_j,N_i*N_j);
-    bool flag = false;
-    iM = gjinverse_alt (M, flag);
-    vector<double> T1 = prod(iM,R);
-
-/*    clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("The time: %f seconds\n", seconds);*/
-
-    matrix<double> z1(N_i,N_j);
-    matrix<double> x_f(N_i,N_j);
-    matrix<double> y_f(N_i,N_j);
-
-    for (int i = 0; i < N_i; i++) {
-        for (int j = 0; j < N_j; j++) {
-            x_f(i,j)=i*dx;
-            y_f(i,j)=j*dy;
-            //printf("%lf  |",T1(i*N_j+j));
-            z1(i,j)=T1(i*N_j+j);
-            //z1 = test(i, j, T1, z1);
-        }
-        //printf("\n");
-    }
-    //printf("\n\n\n");
-    //вывод
-    if (k){
-	    FILE *gp = popen("gnuplot -persist", "w");
-	    fprintf (gp, "set size ratio 0.5\n");
-	    fprintf (gp, "set palette defined (0\"black\",1 \"#191970\",10 \"#00008B\",20 \"#0000FF\",30 \"#6495ED\",40 \"#008080\",50 \"#7FFF00\",60 \"#ADFF2F\",70 \"#FFA500\",80 \"#FF4500\",90 \"#FF0000\",100 \"#8B0000\")\n");
-	    fprintf (gp, "$map2 << EOD\n");
-	    for (int i = 0; i < N_i; i++) {
-	        for (int j = 0; j < N_j; j++) {
-	            fprintf (gp, "%lf %lf %lf\n",x_f(i,j),y_f(i,j),z1(i,j));
-	        }
-	        fprintf (gp, "\n");
-	    }
-	    fprintf (gp, "EOD \n");
-	    fprintf (gp, "plot '$map2' using 1:2:3 with image\n");
+	for (int i = 0; i < N; i++)
+	{ 
+		for (int j = 0; j < N; j++)
+		{
+			matr[i][j] = matrix[i][j]; 
+		}
+			slv[i] = solve[i]; 
 	}
-    return z1;
+
+	// прямой ход Гаусса 
+	#pragma omp parallel for
+	for(int j = 0; j < n; j++)
+	{
+		for (int i = j + 1; i < n; i++)
+		{
+			double koef = matr[i][j] / matr[j][j]; 
+			for(int k = j; k < n; k++)
+			{
+				matr[i][k] -= koef * matr[j][k]; 
+			}
+
+			slv[i] -= koef * slv[j]; 
+		}
+	}
+
+		// обратный ход Гаусса
+	#pragma omp parallel for 
+	for(int i = n - 1; i >= 0; i--)
+	{
+		double sum = 0.0;
+		for(int j = i + 1; j < n; j++)
+		{ 
+			sum += solve[j] * matr[i][j];
+		}
+
+		solve[i] = (slv[i] - sum) / matr[i][i]; 
+	}
+
+	for (int i = 0; i < n; i++) 
+		delete matr[i];
+
+	delete matr; 
 }
 
 
-int main() {
-	omp_set_num_threads(4);
-	printf("Number of threads: %d\n", omp_get_max_threads());
+int inside_tube(int i, int j, int flag)
+{ 
+	if(flag)
+	{
+		if((j >= (X - X_IN) / 2) && (j <= (X + X_IN) / 2) && (i >= (Y - Y_IN) / 2) && (i <= (Y + Y_IN) / 2) )
+		{
+			return 1; 
+		}
+		else
+		{
+			return 0; 
+		}
+	} 
 
-    double a1 = 2;
-    double a2 = 2;
-    double L1 = 12;
-    double L2 = 6;
-   	int N_i = 14;
-	int N_j = 14;
+	else
+	{
+		if((j > (X - X_IN) / 2) && (j < (X + X_IN) / 2) && (i > (Y - Y_IN) / 2) && (i < (Y + Y_IN) / 2) )
+		{
+			return 1; 
+		}
+		else
+		{
+			return 0; 
+		}
+	} 
+}
 
-    matrix<double> z1 = all_calc(a1,a2,L1,L2, N_i, N_j, 1);
-    matrix<double> z2 = all_calc(a1,a2,L1,L2, N_i*2, N_j*2, 1);
+double sss[2];
 
-//Экстраполяция Ричардсона
-	double dx = L1/(N_i - 1);
-    double dy = L2/(N_j - 1);
+int main()
+{
+	//FILE* new_file = fopen("stat", "w");
+	double **matr = new double*[N]; 
 
-    matrix<double> z_R(N_i,N_j);
-    matrix<double> x_R(N_i,N_j);
-    matrix<double> y_R(N_i,N_j);
+	for (int i = 0; i < N; i++) 
+		matr[i] = new double [N]; 
 
-   	for (int i = 0; i < N_i; i++) {
-        for (int j = 0; j < N_j; j++) {
-            x_R(i,j)=i*dx;
-            y_R(i,j)=j*dy;
-            if (i<N_i/2 && j<N_j/2)
-            	z_R(i,j)=(4*z2(2*i, 2*j) - z1(i, j))/3;
-            else if (i>=N_i/2 && j<N_j/2)
-            	z_R(i,j)=(4*z2(2*i+1, 2*j) - z1(i, j))/3;
-            else if (i<N_i/2 && j>=N_j/2)
-            	z_R(i,j)=(4*z2(2*i, 2*j+1) - z1(i, j))/3;
-            else
-            	z_R(i,j)=(4*z2(2*i+1, 2*j+1) - z1(i, j))/3;
+	// заполняем матрицу нулями 
+	for(int i = 0; i < N; ++i)
+	{ 
+		for (int j = 0; j < N; ++j)
+		{
+			matr[i][j] = 0.0; 
+		}
+	}
 
-            //z_R(i,j)=(4*z2(2*i,2*j) - z1(i, j))/3;
-            //z_R(i,j) = z2(2*i,2*j) + (1./3.)*(z2(2*i,2*j)-z1(i,j));
-            //z1 = test(i, j, T1, z1);
-            //printf("%lf |",z_R(i,j));
-        }
-       // printf("\n");
-    }
+	double* solve = new double [N]; 
+	double* x = new double [N]; 
 
-    //вывод
-	    FILE *gp = popen("gnuplot -persist", "w");
-	    fprintf (gp, "set size ratio 0.5\n");
-	    fprintf (gp, "set palette defined (0\"black\",1 \"#191970\",10 \"#00008B\",20 \"#0000FF\",30 \"#6495ED\",40 \"#008080\",50 \"#7FFF00\",60 \"#ADFF2F\",70 \"#FFA500\",80 \"#FF4500\",90 \"#FF0000\",100 \"#8B0000\")\n");
-	    fprintf (gp, "set title 'extrapolation'\n");
-	    fprintf (gp, "$map2 << EOD\n");
-	    for (int i = 0; i < N_i; i++) {
-	        for (int j = 0; j < N_j; j++) {
-	            fprintf (gp, "%lf %lf %lf\n",x_R(i,j),y_R(i,j),z_R(i,j));
-	        }
-	        fprintf (gp, "\n");
-	    }
-	    fprintf (gp, "EOD \n");
-	    fprintf (gp, "plot '$map2' using 1:2:3 with image\n");
+	for(int i = 0; i <= Y; ++i)
+	{ 
+		for (int j = 0; j <= X; ++j)
+		{
+			if( inside_tube(i, j, 1) )
+			{ 
+				if(inside_tube(i, j, 0))
+				{
+				// температура будет решением для уравнения Гаусса во всех точках,
+				// кроме пограничных, где заданы ГУ 3 рода 
+					solve[i * (X + 1) + j] = 0;
+				} 
+				else
+				{
+					solve[i * (X + 1) + j] = 100.0; 
+				}
+			} 
+			else
+			{
+				solve[i * (X + 1) + j] = 10.0; 
+			}
+		} 
+	}
 
-	/*clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("The time: %f seconds\n", seconds);*/
-    return 1;
+	// ГУ первого рода на внутренней стенке. Температура в узлах будет сохраняться
+	for(int i = (Y - Y_IN) / 2; i <= (Y + Y_IN) / 2; ++i)
+	{ 
+		for (int j = (X - X_IN) / 2; j <= (X + X_IN) / 2; ++j)
+		{
+			int k = i * (X + 1) + j; 
+			matr[k][k] = 1;
+		} 
+	}
+
+	// ГУ 3го рода на внешних стенках 
+	for (int j = 1; j < X; ++j)
+	{
+		int i = 0;
+		int k_vnesh = j;
+		int k_vnytr = j + (X + 1); 
+		solve[k_vnesh] = 0; 
+
+		matr[k_vnesh][k_vnesh] = 1 / h_y + 1; 
+		matr[k_vnesh][k_vnytr] = -1 / h_y;
+	} 
+
+	for (int j = 1; j < X; ++j)
+	{ 
+		int i = Y;
+		int k_vnesh = j + (X + 1) * i;
+		int k_vnytr = j + (X + 1) * (i - 1); 
+
+		solve[k_vnesh] = 0; 
+		matr[k_vnesh][k_vnesh] = 1 / h_y + 1; 
+		matr[k_vnesh][k_vnytr] = -1 / h_y;
+	}
+
+	for (int i = 0; i <= Y; ++i)
+	{ 
+		int j = 0;
+		int k_vnesh = i * (X + 1); 
+		int k_vnytr = i * (X + 1) + 1; 
+		solve[k_vnesh] = 0;
+
+		matr[k_vnesh][k_vnesh] = 1 / h_x + 1; 
+		matr[k_vnesh][k_vnytr] = - 1 / h_x;
+	}
+
+	for (int i = 0; i <= Y; ++i)
+	{ 
+		int j = X;
+		int k_vnesh = i * (X + 1) + j;
+		int k_vnytr = i * (X + 1) + j - 1; 
+		solve[k_vnesh] = 0; 
+
+		matr[k_vnesh][k_vnesh] = 1 / h_x + 1; 
+		matr[k_vnesh][k_vnytr] = - 1 / h_x;
+	}
+
+	// Внутренние узлы pаданные с помощью стандартной неявной схемы (по формуле 2)
+	for (int i = 1; i < Y; ++i)
+	{ 
+		for (int j = 1; j < X; ++j)
+		{
+			if( inside_tube(i, j, 1) )
+			{ 
+				continue;
+			}
+			int ij = i * (X + 1) + j;
+			int im1j = (i - 1) * (X + 1) + j; 
+			int ip1j = (i + 1) * (X + 1) + j; 
+			int ijm1 = i * (X + 1) + j - 1; 
+			int ijp1 = i * (X + 1) + j + 1;
+
+			matr[ij][ij] = 2 * ( h_t / (h_x * h_x) + h_t / (h_y * h_y) ) + 1;
+			matr[ij][im1j] = - h_t / (h_y * h_y); 
+			matr[ij][ip1j] = - h_t / (h_y * h_y); matr[ij][ijm1] = - h_t / (h_x * h_x); matr[ij][ijp1] = - h_t / (h_x * h_x);
+			
+			solve[ij] = x[ij]; 
+		}
+	}
+			timespec start, end;
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+	for (int k = 0; k <= TIME / h_t; k+= h_t) 
+	{ 
+		cout << k << endl;
+		// в solve после решения системы уравнений методом Гаусса буде храниться температура трубы.
+		// Чтобы избежать копирования темературы в другой массив, заметим, что в большинстве узлов
+		// получення температура является решением для следующей итерации расчётов.
+		// Температура не является решением только на границах трубы, где заданы ГУ 3-го рода.
+		// В этих узлах в столбце-ответе должен стоять 0, установим его.
+		for (int j = 1; j < X; ++j)
+		{ 
+			solve[j] = 0;
+			solve[j + (X + 1) * Y] = 0; 
+		}
+
+		for (int i = 0; i <= Y; ++i)
+		{ 
+			solve[i * (X + 1)] = 0; 
+			solve[i * (X + 1) + X] = 0;
+		}
+
+		for (int i = 0; i <= Y; ++i){ }
+
+		gauss(matr, solve, N);
+
+		// теперь в solve хранится температура. Записываем её в файл.
+/*		for (int i = 0; i <= Y; i++) 
+		{
+			for (int j = 0; j <= X; j++) 
+			{ 
+				fprintf(new_file, "%3.2lf ", solve[i * (X + 1) + j]); 
+			}
+
+			fprintf(new_file, "\n"); 
+		}
+
+		fprintf(new_file, "\n"); */
+	}
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    printf("Time taken: %lf sec.\n",end.tv_sec-start.tv_sec+ 0.000000001*(end.tv_nsec-start.tv_nsec));
+
+	//fclose(new_file);
+
+	for(int i = 0; i < N; i++)
+	{ 
+		delete matr[i];
+	}
+	delete matr; 
+	delete solve; 
+	delete x; 
+
+	return 0;
 } 
